bcb8f4827bde31a41dbc011636fe3e8f5b501e88
Justin Hughes <jeh5256@gmail.com>
2017-09-27 22:26:07 -0500

    clean up app.js and move code to geocode.js

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const request = require('request');
-
-request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1546%20HIghland%20Ave%20Berwyn%20IL',
-    json: true
-    }, (error, request, body) => {
-        console.log(`Address: ${body.results[0].formatted_address}`);
+const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
+const argv = yargs
+    .options({
+        a : {
+            demnad: true,
+            alias: 'address',
+            description: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+    
+geocode.geocodeAddress(argv.address, (errorMessage, result) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(result, undefined, 2));
+    }
 });
